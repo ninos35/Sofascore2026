30cb7ffb3df3242b3f3d9be8979b043306002554
akademija <akademija@MacBook-Pro-od-akademija.local>
2026-03-05 15:24:33 +0100

    Dodan dependency u novi projekt

---
+++ Sofascore2026/Sofascore2026/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sofascore2026
+//
+//  Created by akademija on 05.03.2026..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
